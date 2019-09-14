086dd7b47b45d0fe44885cedce7a8edc72633e74
rahulreddy <rahulreddy062@gmail.com>
2019-09-14 17:10:43 -0500

    add data validation and sanitizations

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-app-api',{
     useNewUrlParser:true,
@@ -7,19 +8,50 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-app-api',{
 
 const User = mongoose.model('User',{
     name:{
-        type:String
+        type:String,
+        required:true,
+        trim:true
     },
     age:{
-        type:Number
+        type:Number,
+        default:0,
+        validate(value){
+            if(value<0){
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Not valid email ')
+            }
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+        mixLength:7,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Not a valid password')
+            }
+        }
     }
 })
-const me = new User({
-    name:'Rahul',
-    age:27
-})
-
-me.save().then(()=>{
-    console.log(me)
-}).catch((error)=>{
-    console.log(error)
+const Task = mongoose.model('Task',{
+    description:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
 })
