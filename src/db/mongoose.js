81f9692be9afacba852a3a3c98a8a4e295e27b7f
rahulreddy <rahulreddy062@gmail.com>
2019-09-14 18:03:54 -0500

    Create resource reading endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,57 +1,6 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-app-api',{
     useNewUrlParser:true,
     useCreateIndex:true
-})
-
-const User = mongoose.model('User',{
-    name:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    age:{
-        type:Number,
-        default:0,
-        validate(value){
-            if(value<0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Not valid email ')
-            }
-        }
-    },
-    password:{
-        type:String,
-        required:true,
-        trim:true,
-        mixLength:7,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Not a valid password')
-            }
-        }
-    }
-})
-const Task = mongoose.model('Task',{
-    description:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    completed:{
-        type:Boolean,
-        default:false
-    }
 })
